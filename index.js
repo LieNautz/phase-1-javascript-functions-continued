1e2ae6bdeb9f28cc91d60fd205073981aa40a0ca
LieNautz <tuan_lai01@hotmail.com>
2022-08-08 22:13:36 +1000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+// This code allows you to override the default activity 'roller-skate' by simply changing the value within the variable (activity) using string interpolation.
+
+
+let mondayWork= function (office='go to the office') {
+    return `This Monday, I will ${office}.`
+}
+
+//The default value of the variable let= is the function mondayWork.
+//If no other value is passed (inputed) in the function than the default is 'go to office' on mondays.
+//However the code is setup in a way that we can alter the function variable to say mondayWork= function (office='Do not go to office').
+
+function wrapAdjective (whatever= "*") {
+    return function(adjective= "special"){
+        return `You are ${whatever}${adjective}${whatever}!`;
+    }
+}
